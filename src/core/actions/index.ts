f38940b15658fa7f7285d0c2cab483ea97d2878f
bdematt <brandon.dematteis@gmail.com>
2026-03-23 17:29:20 +0100

    feat: repo info action

--- src/core/actions/index.ts
+++ src/core/actions/index.ts
@@ -3,12 +3,14 @@ import utilLog from './util/log.js';
 import orgScratchCreate from './org/scratch/create.js';
 import orgScratchDelete from './org/scratch/delete.js';
 import metadataDeploy from './metadata/deploy.js';
+import githubRepoInfo from './github/repo/info.js';
 
 const actions: Record<string, ActionDefinition> = {
   'util:log': utilLog,
   'org:scratch:create': orgScratchCreate,
   'org:scratch:delete': orgScratchDelete,
   'metadata:deploy': metadataDeploy,
+  'github:repo:info': githubRepoInfo,
 };
 
 export default actions;
