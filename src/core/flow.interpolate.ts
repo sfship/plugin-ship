3278c9b61f8e00f0b9f9d0dbee4de5abf37f5b63
bdematt <brandon.dematteis@gmail.com>
2026-06-27 14:57:46 +0200

    chore: license headers

--- src/core/flow.interpolate.ts
+++ src/core/flow.interpolate.ts
@@ -1,4 +1,19 @@
-/** Matches a value that is exactly one `${{ path }}` token, capturing the path. */
+/*
+ * Copyright 2026, Salesforce, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 const PURE_TOKEN = /^\$\{\{\s*([\w.-]+)\s*\}\}$/;
 /** Matches each `${{ path }}` token embedded within a larger string. */
 const EMBEDDED_TOKEN = /\$\{\{\s*([\w.-]+)\s*\}\}/g;
